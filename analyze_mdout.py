5d089bcaf021131851b14381ccffb1238b27d86f
Ruhi Anjum <ruhishaz8@gmail.com>
2020-07-06 21:42:06 +0530

    add initial project files

---
+++ analyze_mdout.py
@@ -0,0 +1,48 @@
+#open file for reading
+import os
+import argparse
+
+#tell argparse to handle arguments
+parser = argparse.ArgumentParser(description="This script parses amber mdout files to extract Total Energy.")
+
+# tell argparse what Arguments to expect
+parser.add_argument("path", help="The filepath to the file to be analyzed.")
+
+# Get arguments from the user
+args = parser.parse_args()
+
+filename = args.path
+
+f = open(filename,'r')
+
+# Read data in the file.
+data = f.readlines()
+f.close()
+
+# open a new file to write
+base_filename = filename.split('.')
+base_filename = base_filename[0]
+output_filename = F'{base_filename}_Etot.txt'
+
+f_write = open(output_filename,'w+')
+
+print(F'writing output to {output_filename}')
+
+f_write.write('Total Energy\n')
+
+etot = []
+# Loop through lines in the file.
+for line in data:
+    split_line = line.split()
+    
+   # Get information from the line   
+    if 'Etot'in line:
+        etot.append(split_line[2])
+        
+etot = etot[:-2]
+
+for energy in etot:
+    f_write.write(F"{energy}\n")
+        
+
+f_write.close()
